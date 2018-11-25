bff1bd5e2b6a4fdb66695c6970bc5e2f573a02ef
yonaspassos <yona.nantes22@gmail.com>
2018-11-25 19:15:23 -0200

    inicio ao JS e JQuery

---
+++ js/banner.js
@@ -0,0 +1,24 @@
+var banners = ['img/destaque-home.png', 'img/destaque-home-2.png'];
+var bannerAtual = 0;
+
+function trocaBanner() {
+    bannerAtual = (bannerAtual + 1) % 2;
+    document.querySelector('.banner-destaque img').src = banners[bannerAtual];
+
+}
+
+var intervalo = setInterval(trocaBanner, 4000);
+var controle = document.querySelector('.pause');
+
+controle.onclick = function() {
+    if (controle.className == 'pause') {
+        clearInterval(intervalo);
+        controle.className = 'play';
+    } else {
+        intervalo = setInterval(trocaBanner, 4000);
+        controle.className = 'pause';
+        
+    }
+    return false;
+}
+
